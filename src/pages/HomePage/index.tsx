ec576b0974bb6eda8e03a86c223c4f96e42d6fef
Austyn Whaley <austyn_whaley@yahoo.com>
2024-04-28 22:07:02 -0400

    inital commit

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 > DND Worksheet </h1>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
