b06e3eadeaeca91ad80c32b2f43a90543232876b
Austyn Whaley <austyn_whaley@yahoo.com>
2024-04-30 03:02:34 -0400

    api call added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './HomePage.module.css';
+import axios from 'axios';
 
 const HomePage: React.FC = () => {
+
+  const [users, setUsers] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/users');
+                setUsers(response.data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
   return (
   <div className={styles.pageContainer}>
-    <div className="container">
-      <header className="header">
-        <h1 > DND Worksheet </h1>
-      </header>
+    <div className={styles.container}>
+      <h1 > DND Worksheet </h1>
+      <ul>
+          {users.map((user, index) => (
+            <>
+              <li key={index}>{user.name}</li>
+              <li key={index}>{user.email}</li>
+            </>
+                    
+          ))}
+      </ul>
     </div>
   </div>
   );
